1844b5677841076ee66fa45be12065fdf4ca3e3a
praveen-nahar-webkorps <praveen@webkorps.com>
2023-10-20 14:22:17 +0530

    solve cors error

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,4 @@
 class Contact < ApplicationRecord
-    validates :name, presence: true
-    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-    validates :add, presence: true
+    # validates :name, presence: true
+    # validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
