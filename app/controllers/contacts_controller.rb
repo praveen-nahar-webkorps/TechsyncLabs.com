dbd8f22a228abf4a828cbc3d4407f4e841cd2418
praveen-nahar-webkorps <praveen@webkorps.com>
2023-10-31 15:57:25 +0530

    Create google sheet of JobOpeningForm and some updation in Contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,21 +1,23 @@
 class ContactsController < ApplicationController
+  require 'google/apis/sheets_v4'
+  require 'googleauth'
+  
   def create
     @contact = Contact.new(contact_params)
 
     if @contact.save
       begin
-        ContactMailer.admin_email(@contact).deliver_now         # Send email to admin
-        ContactMailer.user_email(params[:email]).deliver_now    # Send email to user
-
+        ContactMailer.admin_email(@contact).deliver_now
+        ContactMailer.user_email(params[:email]).deliver_now
         write_to_google_sheets(@contact)
         
-        render json:{data: @contact, message: "Email sent successfully" }, status: :ok
+        render json: { data: @contact, message: "Email sent successfully" }, status: :ok
       rescue StandardError => e
         render json: { message: "Error sending email to admin: #{e.message}" }, status: :internal_server_error
       end
       
     else
-        render json:{errors: @contact.errors.full_messages },status: :unprocessable_entity
+      render json: { errors: @contact.errors.full_messages }, status: :unprocessable_entity
     end
   end
   
@@ -25,37 +27,21 @@ class ContactsController < ApplicationController
     params.permit(:first_name, :last_name, :email, :phone, :address)
   end
 
-
-  require 'google/apis/sheets_v4'
-  require 'googleauth'
-
   def write_to_google_sheets(contact)
     service = Google::Apis::SheetsV4::SheetsService.new
-    service.client_options.application_name = 'Your Application Name'
+    service.client_options.application_name = 'Google Sheets API'
     service.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
       json_key_io: File.open(Rails.root.join('config', 'users-contact-data-6266e88a0ec0.json')),
       scope: Google::Apis::SheetsV4::AUTH_SPREADSHEETS
     )
   
     spreadsheet_id = '128ZbZG0Rbenmj8Pi-WdHfV13pxPm_Uvuyr1L0OJpIsE'
+    sheet_name = 'ContactsSheet'
+    range = "#{sheet_name}!A:A"
   
-    # Get the last row number with data
-    range = 'A:A'  # Assuming column A is always populated
-    result = service.get_spreadsheet_values(spreadsheet_id, range)
-    last_row = result.values&.size.to_i + 1
-  
-    # Define the range for the new entry
-    range = "A#{last_row}:E#{last_row}"
-  
-    # Prepare the values
     values = [[contact.first_name, contact.last_name, contact.email, contact.phone, contact.address]]
   
-    # Write contact data to the new row
     request_body = Google::Apis::SheetsV4::ValueRange.new(values: values)
-    result = service.update_spreadsheet_value(spreadsheet_id, range, request_body, value_input_option: 'RAW')
-  
-    # puts "Updated #{result.updated_cells} cells."
+    result = service.append_spreadsheet_value(spreadsheet_id, range, request_body, value_input_option: 'RAW', insert_data_option: 'INSERT_ROWS')
   end
-  
-        
 end
--- app/controllers/job_openings_controller.rb
+++ app/controllers/job_openings_controller.rb
@@ -6,6 +6,9 @@ class JobOpeningsController < ApplicationController
       begin
         JobOpeningMailer.admin_email(@jobOpening).deliver_now         # Send email to admin
         JobOpeningMailer.user_email(params[:email]).deliver_now    # Send email to user
+
+        write_to_google_sheets(@jobOpening)
+
         render json:{data: @jobOpening, message: "Email sent successfully" }, status: :ok
       rescue StandardError => e
         render json: { message: "Error sending email to admin: #{e.message}" }, status: :internal_server_error
@@ -19,6 +22,25 @@ class JobOpeningsController < ApplicationController
 
   def jobOpening_params
     params.permit(:first_name, :last_name, :email, :phone, :address, :education, :skills, :total_exp, :personal_quality, :text_info)
+  end
+  
+  def write_to_google_sheets(job_opening)
+    service = Google::Apis::SheetsV4::SheetsService.new
+    service.client_options.application_name = 'Google Sheets API'
+    service.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
+      json_key_io: File.open(Rails.root.join('config', 'users-contact-data-6266e88a0ec0.json')),
+      scope: Google::Apis::SheetsV4::AUTH_SPREADSHEETS
+    )
+  
+    spreadsheet_id = '128ZbZG0Rbenmj8Pi-WdHfV13pxPm_Uvuyr1L0OJpIsE'
+    sheet_name = 'JobOpeningsSheet'  
+    range = "#{sheet_name}!A:A"  
+  
+    values = [[job_opening.first_name, job_opening.last_name, job_opening.email, job_opening.phone, job_opening.address, job_opening.education, job_opening.skills, job_opening.total_exp, job_opening.personal_quality, job_opening.text_info]]
+  
+    request_body = Google::Apis::SheetsV4::ValueRange.new(values: values)
+    result = service.append_spreadsheet_value(spreadsheet_id, range, request_body, value_input_option: 'RAW', insert_data_option: 'INSERT_ROWS')
+  
   end
   
 end
